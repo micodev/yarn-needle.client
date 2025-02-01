e3e52dc32e2d077b0f43bd06d92ec758573bb561
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-01 21:25:44 +0300

    Modified /src/views/OrderHistoryView.vue - February 1, 2025 at 09:25:43 PM

--- src/views/OrderHistoryView.vue
+++ src/views/OrderHistoryView.vue
@@ -75,7 +75,8 @@
               <div
                 class="p-2 bg-gray-100 dark:bg-gray-700 rounded text-center cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-600 flex items-center justify-center gap-2"
                 @click="copyOrderKey(order.key)"
-                v-tooltip.click.top="'تم النسخ'"
+                tabindex="0"
+                v-tooltip.focus.top="'تم النسخ'"
               >
                 <i class="pi pi-copy text-gray-600 dark:text-gray-400"></i>
                 <span class="text-sm text-gray-600 dark:text-gray-400">{{ order.key }}</span>
