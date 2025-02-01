5ddd33d88217593d94b92ed9112c7420ea455a97
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-01 22:06:57 +0300

    Modified /src/views/CoursesView.vue - February 1, 2025 at 10:06:57 PM

--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -33,7 +33,13 @@
             <div class="flex flex-col gap-4 p-4 min-w-[300px] max-h-[80vh] overflow-y-auto">
               <div class="flex-row">
                   <div>
-
+                    <span class="font-medium block mb-2">المجال</span>
+                    <Select v-model="categoryFilter"
+                            :options="categoryOptions"
+                            optionLabel="name"
+                            optionValue="value"
+                            placeholder="اختر المجال"
+                            class="w-full" />
                   </div>
               </div>
 
