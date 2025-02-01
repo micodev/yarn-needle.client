b86192e51653a02939c421076042f2503fe48f72
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-01 22:06:55 +0300

    Modified /src/views/CoursesView.vue - February 1, 2025 at 10:06:55 PM

--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -31,15 +31,9 @@
                   class="whitespace-nowrap" />
           <Popover ref="FilterPopOver">
             <div class="flex flex-col gap-4 p-4 min-w-[300px] max-h-[80vh] overflow-y-auto">
-              <div>
-                  <div class="flex-row">
-                    <span class="font-medium block mb-2">المجال</span>
-                <Select v-model="categoryFilter"
-                         :options="categoryOptions"
-                         optionLabel="name"
-                         optionValue="value"
-                         placeholder="اختر المجال"
-                         class="w-full" />
+              <div class="flex-row">
+                  <div>
+
                   </div>
               </div>
 
