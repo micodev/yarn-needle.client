2c5d3e140c8e3f3b292f7d9bb6b5690769e57b8b
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-02 23:24:48 +0300

    Modified /src/layout/AppFooter.vue - February 2, 2025 at 11:24:48 PM

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -1,11 +1,11 @@
 <template>
-  <footer class="py-4  dark:bg-gray-800 dark:text-white bg-gray-200 text-black transition-colors duration-300">
+  <footer class="py-4 dark:bg-gray-800 dark:text-white bg-gray-200 text-black transition-colors duration-300">
     <div class="container mx-auto px-4">
-      <div class="flex justify-between items-center">
-        <div class="text-sm">
+      <div class="flex flex-col md:flex-row md:justify-between md:items-center space-y-4 md:space-y-0">
+        <div class="text-sm text-center md:text-start">
           &copy; {{ currentYear }} منصة خيط وإبرة. جميع الحقوق محفوظة.
         </div>
-        <div class="flex space-x-4">
+        <div class="flex justify-center md:justify-end space-x-4">
           <Button label="سياسة الخصوصية" class="p-button-secondary p-button-text" />
           <Button label="شروط الخدمة" class="p-button-secondary p-button-text" />
           <Button icon="pi pi-instagram" variant="text" />
