b8db947c9e5b770f3b471d64d53fa7a8aa91fbee
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-03 20:52:32 +0300

    Modified /plugins/axios.plugin.js - February 3, 2025 at 08:52:32 PM

--- plugins/axios.plugin.js
+++ plugins/axios.plugin.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import { API_URL } from '@/config/api'
 
 const axiosInstance = axios.create({
-    baseURL: 'https://localhost:44350',
+    baseURL: API_URL,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json'
