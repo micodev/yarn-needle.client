a8db6b255b8f1b30d453c7d4f351c464d40733ae
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-03 20:12:43 +0300

    Modified /plugins/axios.plugin.js - February 3, 2025 at 08:12:42 PM

--- plugins/axios.plugin.js
+++ plugins/axios.plugin.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'https://localhost:44350/api',
+    baseURL: 'https://localhost:44350/yarn-needle.client/api',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json'
