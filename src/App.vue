447d25aeac5e165f085dbcfcb2105e95df7b9bbf
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-03 20:57:51 +0300

    Modified /src/App.vue - February 3, 2025 at 08:57:51 PM

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import { onMounted } from 'vue'
+import { useAuthStore } from '@/stores/auth'
+
+const authStore = useAuthStore()
+
+onMounted(async () => {
+  await authStore.init()
+})
 </script>
 
 <template>
   <RouterView />
 </template>
+
