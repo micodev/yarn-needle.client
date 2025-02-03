4adc02efb7fdbcdef90901e4dd49a9070c834414
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-03 20:13:59 +0300

    Modified /src/main.js - February 3, 2025 at 08:13:59 PM

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import router from './router'
 import ToastService from 'primevue/toastservice'
 import Toast from 'primevue/toast'
 import Ripple from 'primevue/ripple';
-import axios from '../plugins/axios.plugin'
+import axiosInstance from '../plugins/axios.plugin'
 import Tooltip from 'primevue/tooltip';
 const app = createApp(App)
 const pinia = createPinia();
@@ -25,4 +25,4 @@ app.use(PrimeVue, {
 })
 
 app.mount('#app')
-app.config.globalProperties.$axios = axios
+app.config.globalProperties.$axios = axiosInstance
