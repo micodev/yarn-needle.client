8679eab86609b84167c38f439cc2b1daac063089
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-03 20:24:22 +0300

    Modified /src/stores/auth.js - February 3, 2025 at 08:24:22 PM

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -27,6 +27,7 @@ export const useAuthStore = defineStore('auth', {
         await this.getMe() // Fetch user data after successful login
         return { success: true }
       } catch (error) {
+        console.log(error)
         return { success: false, errors: error.response.data }
       }
     },
