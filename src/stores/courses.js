fe624b31645b6676b53cce1a294d4929b1011354
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-03 12:57:46 +0300

    Modified /src/stores/courses.js - February 3, 2025 at 12:57:45 PM

--- src/stores/courses.js
+++ src/stores/courses.js
@@ -7,7 +7,7 @@ async function fetchCourses() {
   isLoading.value = true;
   // Mimic fetch delay
   await new Promise(resolve => setTimeout(resolve, 1200));
-  const generatedCourses = [
+  courses.value = [
     {
       id: 1,
       title: "التطريز اليدوي للمبتدئين",
@@ -55,11 +55,6 @@ async function fetchCourses() {
       type: 'onsite',
     }
   ];
-  const dynamicCourses = [];
-  for (const course of generatedCourses) {
-    dynamicCourses.push(course);
-  }
-  courses.value = dynamicCourses;
   isLoading.value = false;
 }
 
