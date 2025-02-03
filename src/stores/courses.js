168c0af71724712fe5663b4485140ce05c72cb7e
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-03 12:57:09 +0300

    Modified /src/stores/courses.js - February 3, 2025 at 12:57:09 PM

--- src/stores/courses.js
+++ src/stores/courses.js
@@ -56,7 +56,7 @@ async function fetchCourses() {
     }
   ];
   const dynamicCourses = [];
-  for (const course of [generatedCourses,...generatedCourses]) {
+  for (const course of [generatedCourses]) {
     dynamicCourses.push(course);
   }
   courses.value = dynamicCourses;
