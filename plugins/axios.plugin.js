5c04d7818fc124571b5951d9caec10d469a6c495
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-03 20:53:12 +0300

    Modified /plugins/axios.plugin.js - February 3, 2025 at 08:53:12 PM

--- plugins/axios.plugin.js
+++ plugins/axios.plugin.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-import { API_URL } from '@/config/api'
+
+export const API_URL = import.meta.env.VITE_API_URL
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
