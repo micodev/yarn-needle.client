001e846d7ba4968b79103c90005ef9e5da84b28f
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-05 10:37:23 +0300

    Modified /src/components/ProfileForm.vue - February 5, 2025 at 10:37:23 AM

--- src/components/ProfileForm.vue
+++ src/components/ProfileForm.vue
@@ -309,9 +309,8 @@ onMounted(async () => {
   await countryStore.fetchCountries();
   await nationalityStore.fetchNationalities();
   await membershipStore.fetchMemberships();
-  form.nationalities.push(reactive({ "id": 1, "code": "af", "name": "أفغاني" }))
   await profileStore.fetchProfile();
-  // Object.assign(form, profileStore.getProfile || {});
+  Object.assign(form, profileStore.getProfile || {});
   // //assign nationalities to form.nationalities
   // form.nationalities = reactive(profileStore.getProfile?.nationalities || []);
   // form.nationalities.push(...(profileStore.getProfile?.nationalities || []));
