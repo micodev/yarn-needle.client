c796a0af1a93556b2d02c7dfcde37d662b3c0373
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-05 11:02:30 +0300

    Modified /src/components/ProfileForm.vue - February 5, 2025 at 11:02:30 AM

--- src/components/ProfileForm.vue
+++ src/components/ProfileForm.vue
@@ -311,9 +311,7 @@ onMounted(async () => {
   await membershipStore.fetchMemberships();
   await profileStore.fetchProfile();
   Object.assign(form, profileStore.getProfile || {});
-  // //assign nationalities to form.nationalities
-  // form.nationalities = reactive(profileStore.getProfile?.nationalities || []);
-  // form.nationalities.push(...(profileStore.getProfile?.nationalities || []));
+
 });
 
 const civilianIdError = ref('');
