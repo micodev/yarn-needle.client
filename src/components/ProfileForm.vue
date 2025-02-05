6caf62b6653318aec175e36a126c77034a10e3dc
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-05 21:46:32 +0300

    Modified /src/components/ProfileForm.vue - February 5, 2025 at 09:46:32 PM

--- src/components/ProfileForm.vue
+++ src/components/ProfileForm.vue
@@ -221,8 +221,16 @@
     </template>
   </div>
   <div class="card flex flex-col items-center gap-6">
-        <FileUpload mode="basic" @select="onFileSelect" customUpload auto severity="secondary" class="p-button-outlined" />
-       </div>
+    <FileUpload mode="basic"
+              name="governmentCard"
+              severity="secondary"
+              :multiple="true"
+              class="p-button-outlined w-full"
+              accept="image/*"
+              chooseLabel="إرفاق البطاقة الحكومية"
+              @select="onGovernmentCardUpload"
+           />
+  </div>
 
 </template>
 
