010a10d43c63551485399a6d076d3deb71d08886
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-06 07:40:59 +0300

    Modified /src/layout/AppTopbar.vue - February 6, 2025 at 07:40:59 AM

--- src/layout/AppTopbar.vue
+++ src/layout/AppTopbar.vue
@@ -29,8 +29,8 @@
           <Button :icon="isDarkMode ? 'pi pi-sun' : 'pi pi-moon'" @click="toggleDarkMode" class="h-10" />
           <RegisterForm />
 
-          <Avatar label="TA" shape="circle" class="bg-primary text-white dark:text-black" @click="toggleProfileMenu"
-            aria-haspopup="true" aria-controls="overlay_menu" />
+          <Avatar label="TA" shape="circle" class="bg-primary text-white dark:text-black cursor-pointer"
+            @click="toggleProfileMenu" aria-haspopup="true" aria-controls="overlay_menu" />
           <Menu ref="menu" id="overlay_menu" :model="menuItems" :popup="true" />
         </div>
       </template>
