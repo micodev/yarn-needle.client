60f582109bee816a472a02f5bc0f6b5e6da87ce2
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-06 07:35:51 +0300

    Modified /src/layout/AppTopbar.vue - February 6, 2025 at 07:35:50 AM

--- src/layout/AppTopbar.vue
+++ src/layout/AppTopbar.vue
@@ -29,7 +29,7 @@
           <Button :icon="isDarkMode ? 'pi pi-sun' : 'pi pi-moon'" @click="toggleDarkMode" class="h-10" />
           <RegisterForm />
 
-          <Avatar label="TA" shape="circle" />
+          <Avatar label="TA" shape="circle" class="bg-primary" />
         </div>
       </template>
 
