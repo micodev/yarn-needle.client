e66e8f8652e2a00508d3a27c8156a84651a4e95e
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-07 19:24:09 +0300

    Modified /src/components/LoadingOverlay.vue - February 7, 2025 at 07:24:09 PM

--- src/components/LoadingOverlay.vue
+++ src/components/LoadingOverlay.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center">
     <ProgressSpinner class="w-16 h-16" strokeWidth="4" fill="var(--surface-ground)" />
-    <span class="text-white text-xl mr-4">جاري التحميل...</span>
   </div>
 </template>
 
