714245064cbef684141f008ee66fdd2a9df7c029
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-10 20:02:05 +0300

    Modified /src/views/CourseView.vue - February 10, 2025 at 08:02:05 PM

--- src/views/CourseView.vue
+++ src/views/CourseView.vue
@@ -8,6 +8,7 @@
           <div class="absolute inset-0 right-2 top-2 d-flex flex-col">
             <span class=" bg-gray-50 dark:bg-gray-700 text-gray-900 dark:text-gray-300 py-1 px-2 rounded">{{
               course.isSubscribtionIncluded ?"مشمول بعضويتك":"غير مشمول بعضويتك" }}</span>
+            <span class=" bg-gray-50 dark:bg-gray-700 text-gray-900 dark:text-gray-300 py-1 px-2 rounded">{{ course.subscriptionIncludedNames }}</span>
           </div>
           <!-- <div class="absolute inset-0 flex justify-center items-center bg-black bg-opacity-50">
             <Button icon="pi pi-play" class="p-button-rounded"   severity="primary"  raised />
