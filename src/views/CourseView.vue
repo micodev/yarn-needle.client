1d3284107250449a621a8974100e7bd4f0caf67a
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-10 20:03:53 +0300

    Modified /src/views/CourseView.vue - February 10, 2025 at 08:03:53 PM

--- src/views/CourseView.vue
+++ src/views/CourseView.vue
@@ -5,7 +5,7 @@
       <div class="w-full md:flex-1 flex justify-center items-center md:ml-2 h-auto">
         <div class="relative rounded overflow-hidden w-full">
           <img :src="course.image" alt="Course Thumbnail" class="w-full h-full object-cover rounded">
-          <div class="absolute inset-0 right-2 top-2 d-flex flex-row">
+          <div class="absolute inset-0 right-2 top-2 d-flex flex-col">
             <span class=" bg-gray-50 flex-1 dark:bg-gray-700 text-gray-900 dark:text-gray-300 py-1 px-2 rounded">{{
               course.isSubscribtionIncluded ?"مشمول بعضويتك":"غير مشمول بعضويتك" }}</span>
             <span class=" bg-gray-50 dark:bg-gray-700 text-gray-900 dark:text-gray-300 py-1 px-2 rounded">{{ course.subscriptionIncludedNames }}</span>
