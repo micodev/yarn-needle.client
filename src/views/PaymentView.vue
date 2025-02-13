dd1961815b6cace088d81bf06776dd5e2d9edaee
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-13 22:48:24 +0300

    Modified /src/views/PaymentView.vue - February 13, 2025 at 10:48:24 PM

--- src/views/PaymentView.vue
+++ src/views/PaymentView.vue
@@ -154,8 +154,10 @@
 <script>
 import axios from 'axios'
 //make vue listen to the message event from the iframe and navigate to my-courses
-window.addEventListener('message', () => {
-      window.location.href = '/yarn-needle.client/#/my-courses'
+window.addEventListener('message', (event) => {
+  if (event.origin === 'https://localhost:44350') {
+       window.location.href = '/yarn-needle.client/#/my-courses'
+  }
 })
 export default {
   name: 'PaymentForm',
