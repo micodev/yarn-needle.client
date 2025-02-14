69a980c813a85fe81d5f13b564fa680f2163fa9d
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-14 19:25:51 +0300

    Modified /src/stores/orderStore.js - February 14, 2025 at 07:25:51 PM

---
+++ src/stores/orderStore.js
@@ -0,0 +1,28 @@
+import { defineStore } from 'pinia'
+
+export const useOrderStore = defineStore('order', {
+  state: () => ({
+    isLoading: false,
+    error: null
+  }),
+
+  actions: {
+    async createOrder(courseId, note) {
+      this.isLoading = true
+      this.error = null
+
+      try {
+        const response = await this.$axios.post('/api/order/create', {
+          courseId,
+          note
+        })
+        return response.data
+      } catch (error) {
+        this.error = error.response?.data?.message || 'Failed to create order'
+        throw error
+      } finally {
+        this.isLoading = false
+      }
+    }
+  }
+})
