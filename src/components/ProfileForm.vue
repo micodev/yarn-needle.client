2fce41d3ace67e8a309d96559c8eac2cd624fa14
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-25 21:35:54 +0300

    Modified /src/components/ProfileForm.vue - February 25, 2025 at 09:35:54 PM

--- src/components/ProfileForm.vue
+++ src/components/ProfileForm.vue
@@ -283,7 +283,7 @@
 
 <script setup>
 import { ref, onMounted, computed, nextTick, reactive } from 'vue';
-import { DatePicker, MultiSelect, InputText, Button, IftaLabel, FileUpload, Dropdown } from 'primevue';
+import { DatePicker, MultiSelect, InputText, Button, IftaLabel, FileUpload } from 'primevue';
 import { useProfileStore } from '@/stores/profile';
 import { useCountryStore } from '@/stores/country'; // Add this import
 import { useNationalityStore } from '@/stores/nationality'; // Add this import
