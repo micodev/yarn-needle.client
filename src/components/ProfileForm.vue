cb051921bbdd4c7aee4fd040485aae268c3e44bf
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-02-26 20:28:01 +0300

    Modified /src/components/ProfileForm.vue - February 26, 2025 at 08:28:00 PM

--- src/components/ProfileForm.vue
+++ src/components/ProfileForm.vue
@@ -227,7 +227,7 @@
           <div class="space-y-4">
             <div v-for="(social, index) in form.socialMedia" :key="index"
                  class="flex flex-wrap items-center gap-4 p-4 bg-gray-50 dark:bg-gray-800 rounded-lg">
-              <div class="flex-1">
+              <div class="flex">
                 <IftaLabel>
                   <MultiSelect v-model="social.socialMediaCode"
                            :options="availableSocialMedia"
@@ -240,7 +240,7 @@
                 </IftaLabel>
               </div>
 
-              <div class="flex-2">
+              <div class="flex-1">
                 <IftaLabel>
                   <InputText v-model="social.value"
                             :placeholder="social.code ? `أدخل ${social.name}` : 'أدخل القيمة'"
