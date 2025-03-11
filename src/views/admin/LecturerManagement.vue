ce536f48c931120390aa000e098e63c7daada8d1
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-03-11 22:47:05 +0300

    Modified /src/views/admin/LecturerManagement.vue - March 11, 2025 at 10:47:04 PM

--- src/views/admin/LecturerManagement.vue
+++ src/views/admin/LecturerManagement.vue
@@ -27,7 +27,7 @@
           responsiveLayout="scroll"
         >
           <Column field="id" header="الرقم التعريفي" class="text-center"></Column>
-          <Column header="الاسم" header-style="width: 100%;text-align: center;">
+          <Column header="الاسم" header-class="w-100">
 
             <template #body="{data}">
               <div class="flex items-center">
