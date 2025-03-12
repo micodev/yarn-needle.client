ed9abf98c6677669db4229fa90eb331bac166882
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-03-12 20:27:43 +0300

    Modified /src/views/admin/OrganizationManagement.vue - March 12, 2025 at 08:27:43 PM

--- src/views/admin/OrganizationManagement.vue
+++ src/views/admin/OrganizationManagement.vue
@@ -2,33 +2,21 @@
   <div>
     <h1>Organization Management</h1>
     <!-- ...existing code... -->
-    <div v-if="loading">Loading organizations...</div>
-    <div v-if="error">{{ error }}</div>
-    <ul v-if="organizations && organizations.length">
-      <li v-for="org in organizations" :key="org.id">{{ org.name }}</li>
+    <div v-if="organizationStore.loading">Loading organizations...</div>
+    <div v-if="organizationStore.error">{{ organizationStore.error }}</div>
+    <ul v-if="organizationStore.organizations && organizationStore.organizations.length">
+      <li v-for="org in organizationStore.organizations" :key="org.id">{{ org.name }}</li>
     </ul>
     <!-- ...existing code... -->
   </div>
 </template>
 
-<script>
+<script setup>
 import { onMounted } from 'vue'
 import { useOrganizationStore } from '@/stores/organizationStore'
 
-export default {
-  name: 'OrganizationManagement',
-  setup() {
-    const organizationStore = useOrganizationStore()
-
-    onMounted(() => {
-      organizationStore.fetchOrganizations()
-    })
-
-    return {
-      organizations: organizationStore.organizations,
-      loading: organizationStore.loading,
-      error: organizationStore.error
-    }
-  }
-}
+const organizationStore = useOrganizationStore()
+onMounted(() => {
+  organizationStore.fetchOrganizations()
+})
 </script>
