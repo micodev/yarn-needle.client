a22770eac418fec67a0b0ff74a2707249641c6ea
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-03-12 20:29:12 +0300

    Modified /src/views/admin/OrganizationManagement.vue - March 12, 2025 at 08:29:12 PM

--- src/views/admin/OrganizationManagement.vue
+++ src/views/admin/OrganizationManagement.vue
@@ -4,9 +4,13 @@
     <!-- ...existing code... -->
     <div v-if="organizationStore.loading">Loading organizations...</div>
     <div v-if="organizationStore.error">{{ organizationStore.error }}</div>
-    <ul v-if="organizationStore.organizations && organizationStore.organizations.length">
-      <li v-for="org in organizationStore.organizations" :key="org.id">{{ org.name }}</li>
-    </ul>
+    <!-- Replaced list with cards view -->
+    <div v-if="organizationStore.organizations && organizationStore.organizations.length" class="cards-container">
+      <div v-for="org in organizationStore.organizations" :key="org.id" class="card">
+        <img v-if="org.logo" :src="org.logo" alt="Logo" class="card-logo" />
+        <h2 class="card-title">{{ org.name }}</h2>
+      </div>
+    </div>
     <!-- ...existing code... -->
   </div>
 </template>
