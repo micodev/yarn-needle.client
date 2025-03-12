e3c8d9e4e0b5ba40aff04b4c46f3623e759005ea
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-03-12 15:31:58 +0300

    Modified /src/views/admin/CourseForm.vue - March 12, 2025 at 03:31:58 PM

--- src/views/admin/CourseForm.vue
+++ src/views/admin/CourseForm.vue
@@ -33,9 +33,22 @@
           </div>
           <div class="field col-12 md:col-4">
             <label for="categorySelect" class="block mb-2">المجال*</label>
-            <MultiSelect  id="categorySelect" v-model="selectedCategories" :options="categoryOptions" optionLabel="name" placeholder="اختر المجالات" display="chip" class="w-full" >
-
-              </MultiSelect>
+            <MultiSelect
+              id="categorySelect"
+              v-model="selectedCategories"
+              :options="categoryOptions"
+              optionLabel="name"
+              placeholder="اختر المجالات"
+              display="chip"
+              class="w-full"
+              :pt="{
+                panel: {
+                  header: {
+                    style: { display: 'none' }
+                  }
+                }
+              }"
+            />
           </div>
           <div class="field col-12 md:col-4">
             <label for="originalPrice" class="block mb-2">السعر الأصلي*</label>
