9f6ad46b4b8d493578111a671d80b77d41e98ba5
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-03-21 22:06:44 +0300

    Modified /src/components/CourseCard.vue - March 21, 2025 at 10:06:44 PM

--- src/components/CourseCard.vue
+++ src/components/CourseCard.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    :class="['card p-0 rounded-lg shadow-md relative flex flex-col', 'bg-slate-50 dark:bg-gray-800']"
+    :class="['card p-0 rounded-lg shadow-md relative flex flex-col  h-full', 'bg-slate-50 dark:bg-gray-800']"
   >
-    <div class="relative p-3 sm:p-2 flex-grow h-full">
+    <div class="relative p-3 sm:p-2 flex-grow">
       <img :src="course.image" alt="Course Image" class="w-full h-40 sm:h-36 object-cover rounded" />
       <div class="relative w-full flex flex-col justify-center items-center rounded-md p-2 sm:p-1">
         <p class="text-lg sm:text-base font-bold mb-2 sm:mb-1 text-right text-gray-900 dark:text-gray-100">{{ course.title }}</p>
