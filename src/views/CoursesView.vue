d596b97743c3e397a18f0a6770803335f455cb14
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-03-23 00:00:40 +0300

    Modified /src/views/CoursesView.vue - March 23, 2025 at 12:00:40 AM

--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white dark:bg-gray-900">
+  <div class="">
     <!-- Banner Section -->
     <div class="relative h-[150px] sm:h-[200px] w-full">
       <img src="https://images.unsplash.com/photo-1584992236310-6edddc08acff?q=80&w=1200&h=300&fit=crop" alt="Banner"
