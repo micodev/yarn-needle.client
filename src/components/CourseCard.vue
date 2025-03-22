c579001d714462b12e0d442a23a4871ada4d9046
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-03-22 23:39:35 +0300

    Modified /src/components/CourseCard.vue - March 22, 2025 at 11:39:35 PM

--- src/components/CourseCard.vue
+++ src/components/CourseCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    :class="['card p-0 rounded-lg relative flex flex-col h-full', 'border-2 shadow-inner border-primary-100 dark:border-primary-800 dark:bg-gray-800']"
+    :class="['card p-0 rounded-lg relative flex flex-col h-full', 'border-2 shadow-inner border-primary-100 dark:border-primary-800 ']"
   >
     <div class="relative p-3 sm:p-2 flex-grow">
       <img :src="course.image" alt="Course Image" class="w-full h-40 sm:h-36 object-cover rounded" />
