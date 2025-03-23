f1fcc0de0ea232ce47fa2672c16d3652c205e092
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-03-23 22:28:26 +0300

    Modified /plugins/axios.plugin.js - March 23, 2025 at 10:28:26 PM

--- plugins/axios.plugin.js
+++ plugins/axios.plugin.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-
+const https = require('https');
 export const API_URL = import.meta.env.VITE_API_URL
 export const APP_ENV = import.meta.env.VITE_APP_ENV || 'development'
 
