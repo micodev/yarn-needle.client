9047c8d9769384639cf57d07fe8b283b16178af6
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-03-23 19:34:10 +0300

    Modified /src/views/HomeView.vue - March 23, 2025 at 07:34:10 PM

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -93,7 +93,7 @@ import { useRouter } from 'vue-router';
 import PurchaseConfirmDialog from '../components/PurchaseConfirmDialog.vue';
 import CourseCard from '../components/CourseCard.vue';
 import MembershipCard from '../components/MembershipCard.vue';
-import { VirtualScroller } from "primevue/virtualscroller";
+import  VirtualScroller  from "primevue";
 
 const membershipStore = useMembershipStore();
 const coursesStore = useCoursesStore();
