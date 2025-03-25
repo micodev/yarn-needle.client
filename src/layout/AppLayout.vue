a34d55f4e44a14b8aeb5fe3fbb3ad9d0ba060f5d
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-03-25 22:33:18 +0300

    Modified /src/layout/AppLayout.vue - March 25, 2025 at 10:33:18 PM

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -2,7 +2,7 @@
 
   <div class="layout-wrapper bg-zinc-50 dark:bg-zinc-900 text-gray-900 dark:text-gray-100 relative">
     <wave-background />
-    <app-topbar class="relative z-10"></app-topbar>
+    <app-topbar class="relative z- "></app-topbar>
     <app-sidebar class="relative z-10"></app-sidebar>
     <div class="layout-main-container md:px-12 lg:px-20 relative z-10">
       <div class="layout-main">
@@ -20,3 +20,10 @@ import AppSidebar from './AppSidebar.vue'
 import AppTopbar from './AppTopbar.vue'
 import WaveBackground from '../components/WaveBackground.vue'
 </script>
+
+<style scoped>
+
+.z-2{
+  z-index: 2;
+}
+</style>
