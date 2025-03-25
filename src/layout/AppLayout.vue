ce8f58708288b758cf96755c2563f8781b98f10f
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-03-25 22:14:20 +0300

    Modified /src/layout/AppLayout.vue - March 25, 2025 at 10:14:20 PM

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -1,7 +1,7 @@
 <template>
 
   <div class="layout-wrapper bg-zinc-50 dark:bg-zinc-900 text-gray-900 dark:text-gray-100">
-
+    <wave-background />
     <app-topbar></app-topbar>
     <app-sidebar></app-sidebar>
     <div class="layout-main-container md:px-12 lg:px-20">
@@ -11,7 +11,7 @@
     </div>
     <div class="layout-mask animate-fadein"></div>
   </div>
-  <wave-background />
+
   <Toast />
 </template>
 
