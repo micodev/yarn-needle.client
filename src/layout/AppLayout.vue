e8382a996a3119b1cedaec1738b95ecc2eba018a
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-03-25 22:11:45 +0300

    Modified /src/layout/AppLayout.vue - March 25, 2025 at 10:11:44 PM

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="layout-wrapper bg-zinc-50 dark:bg-zinc-900 text-gray-900 dark:text-gray-100">
+    <wave-background />
     <app-topbar></app-topbar>
     <app-sidebar></app-sidebar>
     <div class="layout-main-container md:px-12 lg:px-20">
@@ -15,4 +16,5 @@
 <script setup>
 import AppSidebar from './AppSidebar.vue'
 import AppTopbar from './AppTopbar.vue'
+import WaveBackground from '../components/WaveBackground.vue'
 </script>
