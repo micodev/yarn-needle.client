706eff222a9f2e7d2aff1bac3098a76a2dc3ff6b
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-03-25 22:17:05 +0300

    Modified /src/layout/AppLayout.vue - March 25, 2025 at 10:17:05 PM

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -3,8 +3,8 @@
   <div class="layout-wrapper bg-zinc-50 dark:bg-zinc-900 text-gray-900 dark:text-gray-100 relative">
     <wave-background />
     <app-topbar class="relative z-10"></app-topbar>
-    <app-sidebar></app-sidebar>
-    <div class="layout-main-container md:px-12 lg:px-20">
+    <app-sidebar class="relative z-10"></app-sidebar>
+    <div class="layout-main-container md:px-12 lg:px-20 relative z-10">
       <div class="layout-main">
         <router-view></router-view>
       </div>
