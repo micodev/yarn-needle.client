abc54281358ad519af846ee99a80aff9efe0e230
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2025-03-28 01:09:54 +0300

    Modified /src/components/RegisterForm.vue - March 28, 2025 at 01:09:53 AM

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -212,9 +212,12 @@ const handleRegister = async () => {
 
 const registerWithGoogleCallback = async (response) => {
   // if enviroment is production, check if the response is valid
-  if (import.meta.env.MODE === 'production') {
+  if (import.meta.env.VITE_APP_ENV === 'production') {
     toast.value.showTemplate('error', 'خطأ: تسجيل الدخول باستخدام Google غير مفعل في بيئة الإنتاج');
     return;
+  }
+  else{
+    alert('تسجيل الدخول باستخدام Google غير مفعل في بيئة الإنتاج');
   }
   loading.value = true;
   try {
